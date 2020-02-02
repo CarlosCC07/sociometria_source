3e2e446d472ff6c59389507ac96ce9a9f6d02352
CarlosCC07 <carlos.ccx10@gmail.com>
2020-02-02 16:55:58 -0600

    Quickfix3

--- sociometria/ajaxShow/analisisGlobalShow.php
+++ sociometria/ajaxShow/analisisGlobalShow.php
@@ -133,7 +133,7 @@
                                 $years = $interval->y;
 				$meses = $interval->m;
 
-				echo"<tr class=\"per".$info[$i]->extra."\" onclick=\"\" ><td style=\"text-align:center;\">".$i+1."</td><td style=\"text-align:right;\">".$info[$i]->idTrabajador."</td><td style=\"text-align:center;\">";
+				echo "<tr class=\"per".$info[$i]->extra."\" onclick=\"\" ><td style=\"text-align:center;\">".$i+1."</td><td style=\"text-align:right;\">".$info[$i]->idTrabajador."</td><td style=\"text-align:center;\">";
 
                                 if($years == 1 && $meses == 1){
                                     echo $years." a. - ".$meses." m.</td>";
