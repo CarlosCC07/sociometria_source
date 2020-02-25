6c914f4ac1ac642bf9f690da8c7241e1a75743f4
CarlosCC07 <carlos.ccx10@gmail.com>
2020-02-24 23:13:44 -0600

    Hotfix6

--- sociometria/init/count.php
+++ sociometria/init/count.php
@@ -1,7 +1,6 @@
 <?php
 
 	$starttime = microtime(true);
-	set_time_limit(600);
 	
     require "../bd/config.php"; //Incluyo la base de datos
 	
@@ -14,117 +13,6 @@
 	global $dbpass;
 	global $dbname;
 	global $dbhost;
-
-	function calculateInd($id,$cont,$level,$type,$dbh){
-		if($level > 7){
-			return;
-		} elseif($level == 1){
-			$cont = 0;
-		}
-
-		if($type == 1){
-			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idAscendencia1 = '$id' OR idAscendencia2 = '$id' OR idAscendencia3 = '$id'";
-			
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute();
-			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
-			$totalInd = count($idInd);
-
-			for($j = 0; $j <$totalInd;$j++){
-				$id2 = $idInd[$j]->idTrabajador;
-				calculateInd($id2,$cont,$level+1,$type,$dbh);
-				$sql="SELECT contAscendencia1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> contAscendencia1;
-				$sql = "UPDATE contadorPersona SET ascendenciaInd = ascendenciaInd + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				
-				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-
-				$sql="SELECT ascendenciaDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> ascendenciaDir;
-				$sql = "UPDATE contadorPersona SET ascendenciaIndAmp = ascendenciaIndAmp + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-			}
-		} elseif ($type == 2) {
-			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idAfinidad1 = '$id' OR idAfinidad2 = '$id' OR idAfinidad3 = '$id'";
-			
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute();
-			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
-			$totalInd = count($idInd);
-
-			for($j = 0; $j <$totalInd;$j++){
-				$id2 = $idInd[$j]->idTrabajador;
-				calculateInd($id2,$cont,$level+1,$type,$dbh);
-				$sql="SELECT contAfinidad1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> contAfinidad1;
-				$sql = "UPDATE contadorPersona SET afinidadInd = afinidadInd + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				
-				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-
-				$sql="SELECT afinidadDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> afinidadDir;
-				$sql = "UPDATE contadorPersona SET afinidadIndAmp = afinidadIndAmp + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-			
-			}
-		} elseif ($type == 3) {
-			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idPopularidad1 = '$id' OR idPopularidad2 = '$id' OR idPopularidad3 = '$id'";
-			
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute();
-			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
-			$totalInd = count($idInd);
-
-			for($j = 0; $j <$totalInd;$j++){
-				$id2 = $idInd[$j]->idTrabajador;
-				calculateInd($id2,$cont,$level+1,$type,$dbh);
-				$sql="SELECT contPopularidad1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> contPopularidad1;
-				$sql = "UPDATE contadorPersona SET popularidadInd = popularidadInd + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				
-				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				
-				$sql="SELECT popularidadDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
-				$cont = $value[0] -> popularidadDir;
-				$sql = "UPDATE contadorPersona SET popularidadIndAmp = popularidadIndAmp + '$cont' WHERE idTrabajador = '$id'";
-				$stmt = $dbh->prepare($sql);
-				$stmt->execute();
-			
-			}
-		}
-	}
 	
 	
 	try {
@@ -236,16 +124,123 @@
 			
 			// INDIRECTOS
 			//************************* ASCENDENCIA
+			
+		
+			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idAscendencia1 = '$id' OR idAscendencia2 = '$id' OR idAscendencia3 = '$id'";
+			
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
+			$totalInd = count($idInd);
+			
+			for($j = 0; $j <$totalInd;$j++){
+				$id2 = $idInd[$j]->idTrabajador;
+				
+				$sql="SELECT contAscendencia1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> contAscendencia1;
+				$sql = "UPDATE contadorPersona SET ascendenciaInd = ascendenciaInd + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				
+				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
 
-			calculateInd($id,0,1,1,$dbh);
+				$sql="SELECT ascendenciaDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> ascendenciaDir;
+				$sql = "UPDATE contadorPersona SET ascendenciaIndAmp = ascendenciaIndAmp + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+
+
+			
+			
+			}
 			
 			//************************* AFINIDAD
+			
+			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idAfinidad1 = '$id' OR idAfinidad2 = '$id' OR idAfinidad3 = '$id'";
+			
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
+			$totalInd = count($idInd);
+			
+			for($j = 0; $j <$totalInd;$j++){
+				$id2 = $idInd[$j]->idTrabajador;
+				
+				$sql="SELECT contAfinidad1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> contAfinidad1;
+				$sql = "UPDATE contadorPersona SET afinidadInd = afinidadInd + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				
+				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
 
-			calculateInd($id,0,1,2,$dbh);
+				$sql="SELECT afinidadDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> afinidadDir;
+				$sql = "UPDATE contadorPersona SET afinidadIndAmp = afinidadIndAmp + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+			
+			}
 			
 			//********************* POPULARIDAD
 			
-			calculateInd($id,0,1,3,$dbh);
+			
+			$sql="SELECT idTrabajador FROM encuestaPersona WHERE idPopularidad1 = '$id' OR idPopularidad2 = '$id' OR idPopularidad3 = '$id'";
+			
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+			$idInd = $stmt->fetchAll(PDO::FETCH_OBJ); // ids de las personas que votaron por el
+			$totalInd = count($idInd);
+			
+			for($j = 0; $j <$totalInd;$j++){
+				$id2 = $idInd[$j]->idTrabajador;
+				
+				$sql="SELECT contPopularidad1 FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> contPopularidad1;
+				$sql = "UPDATE contadorPersona SET popularidadInd = popularidadInd + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				
+				$sql = "UPDATE contadorPersona SET totalIndirecto = totalIndirecto + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				
+				$sql="SELECT popularidadDir FROM contadorPersona WHERE idTrabajador = '$id2'"; // de una persona que voto, saco cuanto tiene en primer lugar de ascendencia
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$value = $stmt->fetchAll(PDO::FETCH_OBJ);
+				$cont = $value[0] -> popularidadDir;
+				$sql = "UPDATE contadorPersona SET popularidadIndAmp = popularidadIndAmp + '$cont' WHERE idTrabajador = '$id'";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+			
+			}
+			
+			
+			
+			
+			
+		
 		}
 		
 		$dbh = null;
